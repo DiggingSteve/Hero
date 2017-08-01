03343f50108043e2e3629ffa05ce3d25e75cbc4b
Yao Dui <doublefly@live.cn>
2017-08-01 18:36:36 +0800

    Signed-off-by: Yao Dui <doublefly@live.cn>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Tour of Heroes';
+    heroes = HEROES;//英雄数组
+    hero: Hero = {
+        id: 1,
+        name: 'Test'
+    };
 }
+
+export class Hero {
+    
+  id: number;
+  name: string;
+}
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+
